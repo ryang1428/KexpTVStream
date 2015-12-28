9b9f2fc119310fa283196ef5dea59635c4cf07a3
Dustin Bergman <dustin.bergman@gmail.com>
2015-12-27 20:19:48 -0500

    Attached Uit o Kexp TV stream

--- KexpTVStream/KexpTVStream/KexpNowPlayingVC.swift
+++ KexpTVStream/KexpTVStream/KexpNowPlayingVC.swift
@@ -10,4 +10,49 @@ import UIKit
 
 class KexpNowPlayingVC: UIViewController {
 
+    @IBOutlet var playPauseButton: UIButton!
+    @IBOutlet var kexpLogo: UIImageView!
+    @IBOutlet var artistNameLabel: UILabel!
+    @IBOutlet var trackNameLabel: UILabel!
+    @IBOutlet var albumNameLabel: UILabel!
+    @IBOutlet var albumArtworkView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addStyleToView()
+
+        AudioManager.sharedInstance.currentTrackUpdate = { [unowned self] (NowPlaying nowPlaying) in
+            self.artistNameLabel.text = nowPlaying.artist
+            self.trackNameLabel.text = nowPlaying.songTitle
+            self.albumNameLabel.text = nowPlaying.album
+            
+         //   self.loadImage(NSURL(string: nowPlaying.albumArtWork!)!)
+        }
+        
+
+    }
+    
+    private func addStyleToView() {
+        let backgroundLayer = KexpStyle.kexpBackgroundGradient()
+        backgroundLayer.frame = view.frame
+        view.layer.insertSublayer(backgroundLayer, atIndex: 0)
+        
+        kexpLogo.layer.cornerRadius = 30.0
+        kexpLogo.clipsToBounds = true
+    }
+
+    @IBAction func playKexpAction(sender: AnyObject) {
+        if (playPauseButton.selected) {
+            playPauseButton.setImage(UIImage(named: "playButton"), forState: .Normal)
+            AudioManager.sharedInstance.pause()
+        }
+        else {
+            playPauseButton.setImage(UIImage(named: "pauseButton"), forState: .Normal)
+            AudioManager.sharedInstance.play()
+        }
+        
+        playPauseButton.selected = !playPauseButton.selected
+    }
+    
 }
---
+++ KexpTVStream/KexpTVStream/KexpStyle.swift
@@ -0,0 +1,52 @@
+//
+//  KexpStyle.swift
+//  KexpTVStream
+//
+//  Created by Dustin Bergman on 12/27/15.
+//  Copyright © 2015 Dustin Bergman. All rights reserved.
+//
+
+import UIKit
+
+let KexpOrangeColorHex = "#FEAC30"
+
+class KexpStyle {
+    class func kexpOrange() -> UIColor {
+        return colorWithHexString(KexpOrangeColorHex)
+    }
+    
+    class func kexpBackgroundGradient () -> CAGradientLayer {
+        let colorTop = KexpStyle.kexpOrange().CGColor
+        let colorBottom = UIColor.whiteColor().CGColor
+        
+        let gl = CAGradientLayer()
+        gl.colors = [ colorTop, colorBottom]
+        gl.locations = [ 0.0, 1.0]
+        
+        return gl
+    }
+    
+    class func colorWithHexString (hex:String) -> UIColor {
+        var cString:String = hex.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()).uppercaseString
+        
+        if (cString.hasPrefix("#")) {
+            cString = (cString as NSString).substringFromIndex(1)
+        }
+        
+        if (cString.characters.count != 6) {
+            return UIColor.grayColor()
+        }
+        
+        let rString = (cString as NSString).substringToIndex(2)
+        let gString = ((cString as NSString).substringFromIndex(2) as NSString).substringToIndex(2)
+        let bString = ((cString as NSString).substringFromIndex(4) as NSString).substringToIndex(2)
+        
+        var r:CUnsignedInt = 0, g:CUnsignedInt = 0, b:CUnsignedInt = 0;
+        NSScanner(string: rString).scanHexInt(&r)
+        NSScanner(string: gString).scanHexInt(&g)
+        NSScanner(string: bString).scanHexInt(&b)
+        
+        
+        return UIColor(red: CGFloat(r) / 255.0, green: CGFloat(g) / 255.0, blue: CGFloat(b) / 255.0, alpha: CGFloat(1))
+    }
+}
